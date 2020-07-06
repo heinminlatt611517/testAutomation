c4ef691816131540606cf0b65183289c6c05a020
nawzarchilinn@Mocs-MacBook-Air-2.local <heinminlatt.dev1@gmail.com>
2020-07-06 23:12:28 +0630

    set up mockburgerModelImpl

---
+++ app/src/main/java/com/zg/burgerjoint/data/model/impls/MockBurgerModelImpl.kt
@@ -0,0 +1,27 @@
+package com.zg.burgerjoint.data.model.impls
+
+import androidx.lifecycle.LiveData
+import com.zg.burgerjoint.data.model.BurgerModel
+import com.zg.burgerjoint.data.vos.BurgerVO
+
+object MockBurgerModelImpl : BurgerModel {
+    override fun getAllBurgers(): LiveData<List<BurgerVO>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun findBurgerById(burgerId: Int): LiveData<BurgerVO> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getBurgersInCart(): LiveData<List<BurgerVO>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun removeItemFromCart(burger: BurgerVO) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun addItemToCart(burger: BurgerVO) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
